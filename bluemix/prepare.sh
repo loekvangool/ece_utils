22e59cf9331c8a1f9f35b094215201eacfaca699
Loek van Gool <loek.vangool@elastic.co>
2017-02-07 13:56:28 +0100

    Create prepare.sh

---
+++ bluemix/prepare.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+id -u centos &>/dev/null || sudo useradd centos; PASS=`openssl rand -base64 8`; echo $PASS | passwd --stdin centos; echo 'centos  ALL=(ALL:ALL) ALL' >> /etc/sudoers; echo "Write down centos password: ${PASS}"; PASS=; sleep 3; su centos; cd ~;
